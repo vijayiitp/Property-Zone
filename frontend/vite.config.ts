8f03f978640a56a104ac380bf082df39c3df67f2
Vijay Kumar <139329029+vijayiitp@users.noreply.github.com>
2025-09-02 18:33:59 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+  // This ensures frontend routing works on refresh in dev
+  preview: {
+    port: 4173,
+    strictPort: true,
+  }
+});
